96f2fc13dd66e7c4b9e55c1680a8cbf8f0b24816
mwfister <mwfister@gmail.com>
2016-09-08 16:37:13 +1000

    Heroku Tut

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+const App = (props) => {
+  return (
+    <div>
+      <h1> Hello World </h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  entry: [
+    "./src/index.js"
+  ],
+  output: {
+    path: __dirname,
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  loaders: [{
+    test: /\.jsx?$/,
+    exclude: '/node_modules/',
+    loader: babel-loader,
+    query: {
+      presets: ['es2015', 'react']
+    }
+  }],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
