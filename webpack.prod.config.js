0a1a4e6b4945f5e48935a8afb1aa6337adeb6549
mwfister <mwfister@gmail.com>
2016-09-11 14:29:38 +1000

    Localhost working (dev env)

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -23,7 +23,7 @@ module.exports = {
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
+      minimize: false,
       compress: {
         warnings: false
       }
